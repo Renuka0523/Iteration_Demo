72b9f6714df52ef794d01b7dc8e7b04b27dd33e4
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-11-29 21:45:06 +0530

    Add Changes into Ieration demo

--- Iteration_Demo/Program.cs
+++ Iteration_Demo/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for(var i = 0; i <= 10; i++) 
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+            
+            }
+
+            for (var i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            for (var i = 10; i > 1; i--)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            for (var i = 10; i > 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
